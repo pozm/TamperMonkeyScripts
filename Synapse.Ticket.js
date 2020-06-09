9be4ed58b6283f501aaa596a3cef8cad1437048d
pozm <44528100+pozm@users.noreply.github.com>
2020-06-09 13:28:55 +0100

    Updated version

--- Synapse.Ticket.js
+++ Synapse.Ticket.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Synapse support ticket master.
 // @namespace    http://tampermonkey.net/
-// @version      2.0
+// @version      2.1
 // @description  Title
 // @author       Pozm
 // @updateURL    https://raw.githubusercontent.com/pozm/TamperMonkeyScripts/master/Synapse.Redirect.js
